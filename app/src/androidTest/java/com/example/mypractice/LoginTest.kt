c5ca2c548f3c1d47761b30c11cec057cd8813d19
KnobOperator <KnobOperator2@proton.me>
2024-05-10 15:58:26 +0200

    Update LoginTest
    Adding login failure test.
    Issue #5

--- app/src/androidTest/java/com/example/mypractice/LoginTest.kt
+++ app/src/androidTest/java/com/example/mypractice/LoginTest.kt
@@ -2,12 +2,14 @@ package com.example.mypractice
 
 import androidx.test.espresso.Espresso.closeSoftKeyboard
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Root
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import org.junit.Assert.*
@@ -34,7 +36,21 @@ class LoginTest
         Thread.sleep(1000)
         onView(withId(R.id.login_activity)).check(doesNotExist())
 
-//        onView(withText("Invalid login details, please try again")).inRoot(ToastMatcher())
-//            .check(matches(isDisplayed()))
+    }
+    
+    @Test
+    fun testLoginFailure(){
+        onView(withId(R.id.etEmail)).perform(click())
+        onView(withId(R.id.etEmail)).perform(typeText("invalid@gmail.com"))
+        closeSoftKeyboard()
+        onView(withId(R.id.etPassword)).perform(click())
+        onView(withId(R.id.etPassword)).perform(typeText("invalid"))
+        closeSoftKeyboard()
+        onView(withId(R.id.btnLogin)).perform(click())
+
+        Thread.sleep(1000)
+        onView(withId(R.id.login_activity)).check(matches(isDisplayed()))
     }
 }
+
+
